b7c3180ce6fb8434a73e90e15f071070d3dd0662
BodyaX <abaharev340@gmail.com>
2026-09-18 21:56:17 +0500

    add function database protection

--- Sferum_bot/Files/database.py
+++ Sferum_bot/Files/database.py
@@ -5,11 +5,12 @@ from fastapi import FastAPI
 storage = 'Files'
 os.makedirs(storage, exist_ok=True) # Создаем папку, если ее нет
 
-# Настройка бд
+# Подключаемся к бд и создаем таблицу для метаданных.
 db = sqlite3.connect('my_bot.db')
 sql = db.cursor()
 sql.execute("""
 create table if not exists files ( 
+    id INTEGER PRIMARY KEY AUTOINCREMENT
     name TEXT,
     file_path TEXT,
     uid TEXT,
@@ -19,29 +20,36 @@ create table if not exists files (
 db.commit()
 db.close() 
 
-#создаем сервер
+#запуск сервера
 app = FastAPI(title="Server")
 
+# Функция  срабатывает, когда бот или клиент шлет пост запрос 
 @app.post("/upload/")
 def upload_file(request_data: dict):
     user_id = request_data["uid"]
-    file_name = request_data["name"]
-    file_ext = request_data["extension"]
+
+    # Вырезаем слеши из имени и расширения, защита от атаки
+    file_name = request_data["name"].replace("/", "").replace("\\", "")
+    file_ext = request_data["extension"].replace("/", "").replace("\\", "")
     file_content = request_data["data"]
-    
+
+    # Склеиваем безопасное имя и путь
     full_file_name = f"{file_name}.{file_ext}"
     file_path = f"{storage}/{full_file_name}"
     
-    # 1. Сохраняем файл на диск
+    # Открываем файл для записи
+    # with - безопасно закроет файл в конце
+    # utf-8 - чтобы русские буквы из конспекта не стали иероглифами
     with open(file_path, "w", encoding="utf-8") as buffer:
-        buffer.write(file_content)
+        buffer.write(file_content) #закрытие диска
         
-    # 2. Пишем в базу данных (ВНУТРИ ФУНКЦИИ!)
     # Конструкция with сама откроет базу, сама сделает commit при успехе и сама её закроет
     with sqlite3.connect('my_bot.db') as local_db:
+        # Записываем данные в таблицу.
+        # Это защита от SQL-инъекций (чтобы хакер не смог удалить таблицу через имя файла)
         local_db.execute(
-            "INSERT INTO files (name, uid, file_path) VALUES (?, ?, ?)", 
-            (full_file_name, user_id, file_path)
+            "INSERT INTO files (name, extension, uid, file_path) VALUES (?, ?, ?, ?)", 
+            (full_file_name, file_ext, user_id, file_path)
         )
         
     return {"status": "success", "message": f"Конспект {full_file_name} успешно сохранен!"}
